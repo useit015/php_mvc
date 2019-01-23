83c05a87fb45d1e8a8ed4dd8824ce69eb3f61b91
Oussama Nahiz <onahiz@e1r8p11.1337.ma>
2019-01-23 19:25:58 +0100

    initial commit

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+
+// Url Root
+define('URLROOT', 'http://localhost:8100/phpmvc');
+
+// Site Name
+define('SITENAME', 'phpmvc');
+
+?>
